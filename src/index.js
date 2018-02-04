e02ea01325985eccfe1cf5bd09e0a27b4e52b17a
sestienney <sebastien.estienney@adminext.com>
2018-02-04 11:43:02 +0100

    regular commit dimanche 4 février 2018, 11:43:02 (UTC+0100)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 'use strict';
 
-console.log('init');
+const express = require('express');
+const nconf = require('nconf');
+
+nconf.argv().env().file('conf.json').defaults({
+    PORT: 3000
+});
+
+
+const app = express();
+app.use(function (err, req, res, next) {
+    console.error(err.stack);
+    next(err);
+});
+app.use(function (err, req, res, next) {
+    res.status(500).send({error: err.message});
+});
+app.listen(nconf.get('PORT'), () => {
+    console.log('web server started');
+});
---
+++ src/routes/api.private.route.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+const productController = require('../controllers/product.controller');
+
+// router.get('/product/initFromCsv', productController.initFromCsv);
+
+module.exports = router;
